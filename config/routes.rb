11d2be8868ea0997ca0ea5d4363246ca828a0192
David Goldberg <drgold1988@gmail.com>
2014-12-08 23:04:53 -0800

    Routes!

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
-Rails.application.routes.draw do
+Agile::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root 'store#index', as: 'store'
   # root 'welcome#index'
 
   # Example of regular route:
