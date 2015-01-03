cb73ce0f4492c45978151f3881821581284b481a
David Goldberg <drgold1988@gmail.com>
2015-01-02 21:34:22 -0800

    Agile-A7
    Count Items in Shopping Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,8 @@ class Product < ActiveRecord::Base
   
   before_destroy :ensure_not_referenced_by_any_line_item
   
-  private 
+  private
+  
   def ensure_not_referenced_by_any_line_item
     if line_items.empty?
       return true
